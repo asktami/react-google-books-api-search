aa3e17851c46dd681fc8dd03e272023b3c4a1b46
Tami Williams <tami@asktami.com>
2019-08-23 11:21:50 -0400

    Updates

---
+++ precache-manifest.ef7551a3715b99b1573b484b42f919f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5f644299df0b3c898677958fc9183f",
+    "url": "/react-google-books-api-search/index.html"
+  },
+  {
+    "revision": "6f2c23080ff6695648b6",
+    "url": "/react-google-books-api-search/static/css/main.86858196.chunk.css"
+  },
+  {
+    "revision": "ea99919e01c0051c973a",
+    "url": "/react-google-books-api-search/static/js/2.105aead3.chunk.js"
+  },
+  {
+    "revision": "6f2c23080ff6695648b6",
+    "url": "/react-google-books-api-search/static/js/main.e192d6ad.chunk.js"
+  },
+  {
+    "revision": "fc7d9e7d3b1989056d77",
+    "url": "/react-google-books-api-search/static/js/runtime~main.e0bb40fc.js"
+  },
+  {
+    "revision": "a1499d5c8e8a653f13ca66ffe71c7259",
+    "url": "/react-google-books-api-search/static/media/ria-puskas-OobxlWxuLaA-unsplash.a1499d5c.jpg"
+  },
+  {
+    "revision": "d63cd00370a02c44e53a97e3db3668a1",
+    "url": "/react-google-books-api-search/static/media/wall.d63cd003.png"
+  }
+]);
