babd462584e5d030ed7d536d867f9fdb319b83e2
Tami Williams <tami@asktami.com>
2019-08-23 20:24:43 -0400

    Added noopnere and noreferrer to button link

--- src/ItemButtons.js
+++ src/ItemButtons.js
@@ -4,7 +4,7 @@ export class ItemButtons extends React.Component {
 	render() {
 		return (
 			<div className="item-buttons">
-				<a href={this.props.link} target="_blank">
+				<a href={this.props.link} target="_blank" rel="noopener noreferrer">
 					view
 				</a>
 			</div>
